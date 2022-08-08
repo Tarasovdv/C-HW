9db34b5fd9616a5bf45b8fca6fffe0c40ed4c2f9
Dima Tarasov <desant74268@yandex.ru>
2022-08-08 12:44:28 +0300

    add Task1,max/min. 2values

--- HW1/Task1/Program.cs
+++ HW1/Task1/Program.cs
@@ -4,3 +4,22 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
+Console.Clear();
+Console.WriteLine("Task #1");
+Console.WriteLine("Enter first number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    int max = a;
+    int min = b;
+    Console.WriteLine("a = " + a + "; " + "b = " + b + " -> " + "max = " + max + "; " + "min = " + min);
+}
+else
+{
+    int max = b;
+    int min = a;
+    Console.WriteLine("a = " + a + "; " + "b = " + b + " -> " + "max = " + max + "; " + "min = " + min);
+}
