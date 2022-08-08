20dede3ab2cbdd16608bf8472b1597e3b0b2d514
Dima Tarasov <desant74268@yandex.ru>
2022-08-08 13:02:21 +0300

    add Task2,max. 3values

---
+++ HW1/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Clear();
+Console.WriteLine("Task #2");
+Console.WriteLine("Enter three numbers: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("\na = " + a + "\n" +
+                  "b = " + b + "\n" +
+                  "c = " + c + "\n" +
+                  "max = " + max);
+
