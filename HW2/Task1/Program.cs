73a350cfb6b01f1d1bf19edf6fbe960ae5ac6866
Dima Tarasov <desant74268@yandex.ru>
2022-08-11 09:10:39 +0300

    add HW2 Task1

---
+++ HW2/Task1/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+Console.WriteLine("Task #1: ");
+
+void Greeting() // Приветствие
+{
+    Console.WriteLine("Enter a three-digit number: ");
+}
+int Enter() // Ввод числа
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("\nnum = " + num);
+    return num;
+}
+bool CheckNum(int num) // Проверка числа на трехзначность
+{
+    if ((num / 100 >= 1) && !(num / 100 >= 10)) return true;
+    else
+    {
+        Console.WriteLine("You entered an incorrect number/ please repeat\n");
+        return false;
+    }
+}
+int TwoDigitCalculation(int num) // Вычисление второго знака
+{
+    int twoDidit = (num % 100) / 10;
+    return twoDidit;
+}
+
+bool check = false;
+while (!check)
+{
+    Greeting();
+    int num = Enter();
+    check = CheckNum(num);
+    if (check)
+    {
+        Console.WriteLine("Two digit value:\n" + num + " -> " + TwoDigitCalculation(num));
+
+    }
+}
+
+
+
