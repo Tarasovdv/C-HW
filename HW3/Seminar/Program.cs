cb13d8222b870f4da9b5570bc49da949c348c0a7
Dima Tarasov <desant74268@yandex.ru>
2022-08-17 22:06:59 +0300

    add HW3 Seminar

---
+++ HW3/Seminar/Program.cs
@@ -0,0 +1,21 @@
+﻿// напишите прогр.кот на вход принимает координату (XY)
+// и выдает номер четверти
+
+// Console.Clear();
+// Console.WriteLine("Введите координату OX: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите координату OY: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+// if (x > 0 && y > 0) Console.WriteLine("First");
+// else if (x < 0 && y > 0) Console.WriteLine("Second");
+// else if (x < 0 && y < 0) Console.WriteLine("Third");
+// else if (x > 0 && y < 0) Console.WriteLine("Fourth");
+// else Console.WriteLine("Error 404, quarter not found");
+
+
+double test = 1.721;
+Console.WriteLine($"test = {Math.Round(test,1)}");
+
+
