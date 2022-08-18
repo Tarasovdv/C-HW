9164f43c53ce36d0451a04556c217bea2552af54
Dima Tarasov <desant74268@yandex.ru>
2022-08-18 13:38:02 +0300

    add HW3 Task3

---
+++ HW3/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.WriteLine("Enter the number");
+double num = Convert.ToDouble(Console.ReadLine());
+double powNum;
+int count = 1;
+
+while (count <= num)
+{
+    powNum = Math.Pow(count, 3);
+    Console.WriteLine($"{count}^3 -> {powNum}");
+    count++;
+}
