0515e10921e3d3486a13c81c8a765e28e50640f4
Dima Tarasov <desant74268@yandex.ru>
2022-08-19 17:55:46 +0300

    add HW3 Task2

---
+++ HW4/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Enter the number: ");
+string number = Console.ReadLine();
+
+int length = number.Length;
+int sum = 0;
+// for (int i = 0; i < length; i++)
+// {
+//     Console.WriteLine(number[i]);
+//     string test = number[i].ToString();
+//     int testInt = Convert.ToInt32(number[i].ToString());
+//          Console.WriteLine("test" + test);
+//          Console.WriteLine("testINT" + testInt);
+//     // int value = Convert.ToInt32(number[i]);
+//     // Console.WriteLine(value);
+// }
+// // Console.WriteLine(sum);
+
+for (int i = 0; i < length; i++)
+{
+    int value = Convert.ToInt32(number[i].ToString());
+    sum += value;
+}
+Console.WriteLine(sum);
