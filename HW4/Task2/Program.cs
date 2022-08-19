ef6b6a63f57cf353301daed7ab56ff217908f388
Dima Tarasov <desant74268@yandex.ru>
2022-08-19 17:57:41 +0300

    add HW3 Task2

--- HW4/Task2/Program.cs
+++ HW4/Task2/Program.cs
@@ -9,18 +9,6 @@ string number = Console.ReadLine();
 
 int length = number.Length;
 int sum = 0;
-// for (int i = 0; i < length; i++)
-// {
-//     Console.WriteLine(number[i]);
-//     string test = number[i].ToString();
-//     int testInt = Convert.ToInt32(number[i].ToString());
-//          Console.WriteLine("test" + test);
-//          Console.WriteLine("testINT" + testInt);
-//     // int value = Convert.ToInt32(number[i]);
-//     // Console.WriteLine(value);
-// }
-// // Console.WriteLine(sum);
-
 for (int i = 0; i < length; i++)
 {
     int value = Convert.ToInt32(number[i].ToString());
