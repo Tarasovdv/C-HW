b68f857425fd48d2ac528a547149438257d80b4e
Dima Tarasov <desant74268@yandex.ru>
2022-08-23 21:46:42 +0300

    add HW5 Task2

---
+++ HW5/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+Console.WriteLine("Задача 36:");
+
+
+int[] array = new int[6];
+int arrSize = array.Length;
+int sumOddPositionValue = 0;
+
+for (int i = 0; i < arrSize; i++)
+{
+    array[i] = new Random().Next(5);
+}
+
+
+for (int j = 1; j < arrSize; j += 2)
+{
+    sumOddPositionValue += array[j];
+}
+
+Console.WriteLine($"Array [ {String.Join("; ", array)} ] -> {sumOddPositionValue}");
