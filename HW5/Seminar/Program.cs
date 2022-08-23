1c50f17ff2a23cb002fff788d54e1f5e0e364073
Dima Tarasov <desant74268@yandex.ru>
2022-08-23 21:47:28 +0300

    add HW5 Seminar

---
+++ HW5/Seminar/Program.cs
@@ -0,0 +1,23 @@
+﻿// 31.
+
+Console.Clear();
+
+int[] arr = new int[12];
+int resultPositive = 0;
+int resultNegative = 0;
+int arraySize = arr.Length;
+
+for (int i = 0; i < arraySize; i++)
+{
+    arr[i] = new Random().Next(-9, 10);
+    // Console.Write(arr[i] +", ");
+}
+
+Console.WriteLine($"Array: [ {String.Join("; ", arr)} ]");
+
+for (int j = 0; j < arraySize; j++)
+{
+    if (arr[j] < 0) resultNegative += arr[j];
+    else resultPositive += arr[j];
+}
+Console.WriteLine($"ResultPositive: {resultPositive} \nResultNegative: {resultNegative}");
