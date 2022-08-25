30d86eeaf4bbb74c002d24ceb5025157d3a0ee86
Dima Tarasov <desant74268@yandex.ru>
2022-08-25 23:03:25 +0300

    add HW6 Seminar

---
+++ HW6/Seminar/Program.cs
@@ -0,0 +1,34 @@
+﻿// переворот массива
+
+
+// int[] array = new int[5];
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(0, 11);
+// }
+
+// Console.WriteLine($"Array       [ {String.Join("; ", array)} ]");
+
+// int[] resultArray = new int[array.Length];
+
+// int lastIndex = array.Length - 1;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     resultArray[i] = array[lastIndex - i];
+// }
+// Console.WriteLine($"ResultArray [ {String.Join("; ", resultArray)} ]");
+
+// существует ли треугольник
+
+Console.Clear();
+Console.WriteLine("Enter 1 side");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter 2 side");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter 3 side");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a + b > c && b + c > a && a + c > b) Console.WriteLine("Треугольник существует");
+else Console.WriteLine("Треугольник НЕ существует");
+    
